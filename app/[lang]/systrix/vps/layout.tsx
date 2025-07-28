3fc1df650ab06f4e00b3355daec17cd04a318171
Matěj Štipčák <46084722+matk0shub@users.noreply.github.com>
2025-07-28 13:54:39 +0200

    refactor: convert systrix VPS page to CSS module layout

---
+++ app/[lang]/systrix/vps/layout.tsx
@@ -0,0 +1,4 @@
+import './systrix-theme.module.css';
+export default function SystrixLayout({ children }: { children: React.ReactNode }) {
+  return <div className="systrix-theme">{children}</div>;
+}
